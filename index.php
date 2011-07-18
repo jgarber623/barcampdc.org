989ca53f2a4666a9f8239de7e4f4ee864882a529
Jason Garber <jason@sixtwothree.org>
2011-07-18 15:04:27 -0400

    Adding version 3 of BarCamp DC site to repository.

--- index.php
+++ index.php
@@ -1,108 +1,101 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-	<title>BarCamp DC | a technology unconference</title>
-	<meta name="author" content="BarCamp DC" />
-	<meta name="description" content="BarCamp comes to the nation's capital Saturday, August 11th, 2007." />
-	<meta name="keywords" content="barcamp dc, barcamp washington dc, barcamp, unconference, washington dc tech events, washington dc events, technology" />
-	<meta name="robots" content="all" />
-	<meta name="geo.position" content="38.903762;-77.059765" />
-	<meta name="geo.region" content="US-DC" />
-	<meta name="geo.placename" content="Washington DC" />
-	<meta name="geo.country" content="US" />
-	<link rel="stylesheet" type="text/css" media="screen" href="css/barcamp.css" />
-	<script type="text/javascript" src="http://maps.google.com/maps?file=api&amp;v=2&amp;key=ABQIAAAA4vs3EKzQQ5215QbVI-kWVhS1rteLo4F7_IcxunjOqbSz4U7yBxRyoy8WuKFh6GsjkvVRsujUkTieGA"></script>
-	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.2.6/jquery.min.js"></script>
-	<script type="text/javascript" src="scripts/barcampdc.js"></script>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<title>BarCamp DC</title>
+<link rel="icon" href="favicon.ico" type="image/x-icon">
+<link rel="stylesheet" type="text/css" media="all" href="css/reset.css" />
+<link rel="stylesheet" type="text/css" media="all" href="css/960.css" />
+<link rel="stylesheet" type="text/css" media="all" href="css/screen.css" />
 </head>
-
 <body>
+<div class="container_12">
 
-	<div id="wrap">
-		<div id="header">
-			<h1><img src="images/logo.gif" alt="BarCamp DC" /></h1>
-		</div>
-		<div id="content">
-			<div id="about">
-				<dl class="vevent">
-					<dt id="what">What</dt>
-						<dd><span class="summary">BarCamp DC</span> is an ad-hoc <a href="http://en.wikipedia.org/wiki/Unconference">unconference</a> and is part of the larger <a href="http://barcamp.org/">BarCamp</a> movement. While there are topics suggested for discussion, there is no set agenda until everyone gathers. Attendees are required to participate to some degree: either by giving a presentation, demo, etc. or by facilitating a group discussion. Topics are flexible, as well.</dd>
-					<dt id="when">When</dt>
-						<dd>Saturday, October 18th, 2008, <abbr class="dtstart" title="2008-10-18T08:00:00Z-0500">8:00 AM</abbr> &ndash; <abbr class="dtend" title="2008-10-18T18:00:00Z-0500">6:00 PM</abbr></dd>
-					<dt id="where">Where</dt>
-						<dd class="vcard">
-							<a href="http://www.cdiabu.com/overview/washington-campus.php" class="fn org url">Center for Digital Imaging Arts at Boston University</a>
-							<span class="adr">
-								<span class="street-address">1055 Thomas Jefferson Street NW</span>
-								<span class="locality">Washington, <abbr class="region" title="District of Columbia">DC</abbr></span> <span class="postal-code">20007</span>
-								<span class="geo">
-									<span class="latitude">38.903762</span>, <span class="longitude">-77.059765</span>
-								</span>
-							</span>
-						</dd>
-				</dl>
-			</div>
-			
-			<div id="register" class="box">
-				<h3>Attend</h3>
-				<p>Want to come to BarCamp DC? Simply register at <a href="http://barcampdc2.eventbrite.com/">http://barcampdc2.eventbrite.com/</a>.</p>
-			</div>
-			
-			<div id="sponsors" class="box">
-				<h3>Sponsors</h3>
-				<ul>
-					<li class="vcard"><a href="http://1piazza.com/" class="fn org url">1Piazza</a>,</li>
-					<li class="vcard"><a href="http://www.au.org/" class="fn org url">Americans United for Separation of Church and State</a>,</li>
-					<li class="vcard"><a href="http://www.ask500people.com/" class="fn org url">Ask500People</a>,</li>
-					<li class="vcard"><a href="http://www.awayfind.com/" class="fn org url">AwayFind</a>,</li>
-					<li class="vcard"><a href="http://blackmesh.com/" class="fn org url">BlackMesh</a>,</li>
-					<li class="vcard"><a href="http://caseysoftware.com/" class="fn org url">CaseySoftware, LLC</a>,</li>
-					<li class="vcard"><a href="http://www.collectivedetective.com/" class="fn org url">Collective Detective</a>,</li>
-					<li class="vcard"><a href="http://www.contribune.org/" class="fn org url">Contribune</a>,</li>
-					<li class="vcard"><a href="http://www.designreinvented.com/" class="fn org url">Design Reinvented</a>,</li>
-					<li class="vcard"><a href="http://friendcompare.com/" class="fn org url">FriendCompare</a>,</li>
-					<li class="vcard"><a href="http://intridea.com/" class="fn org url">Intridea</a>,</li>
-					<li class="vcard"><a href="http://www.introin.com/" class="fn org url">IntroIn</a>,</li>
-					<li class="vcard"><a href="http://istrategylabs.com/" class="fn org url">iStrategyLabs</a>,</li>
-					<li class="vcard"><a href="http://www.iterativedesigns.com/" class="fn org url">Iterative Designs</a>,</li>
-					<li class="vcard"><a href="http://jess3.com/" class="fn org url">Jess3</a>,</li>
-					<li class="vcard"><a href="http://jobmatchbox.jobthread.com/" class="fn org url">Jobmatchbox.com</a>,</li>
-					<li class="vcard"><a href="http://www.jfciii.com/blog/" class="fn url">John F Croston III</a>,</li>
-					<li class="vcard"><a href="http://jounce.net/" class="fn org url">Jounce</a>,</li>
-					<li class="vcard"><a href="http://www.lesliebradshaw.com/" class="fn url">Leslie Bradshaw</a>,</li>
-					<li class="vcard"><a href="http://m.odul.us/" class="fn org url">Matt Bowen</a>,</li>
-					<li class="vcard"><a href="http://www.networksolutions.com/" class="fn org url">Network Solutions</a>,</li>
-					<li class="vcard"><a href="http://openhost.biz/" class="fn org url">OpenHost</a>,</li>
-					<li class="vcard"><a href="http://www.positiveenergyusa.com/" class="fn org url">Positive Energy</a>,</li>
-					<li class="vcard"><a href="http://potomactechwire.com/" class="fn org url">PotomacTechWire</a>,</li>
-					<li class="vcard"><a href="http://sharememe.com/" class="fn org url">ShareMeme</a>,</li>
-					<li class="vcard"><a href="http://siteworx.com/" class="fn org url">Siteworx</a>,</li>
-					<li class="vcard"><a href="http://www.viget.com/" class="fn org url">Viget Labs</a>,</li>
-					<li class="vcard"><a href="http://www.welovedc.com/" class="fn org url">We Love DC</a>,</li>
-					<li class="vcard"><a href="http://www.wondermill.com/" class="fn org url">Wondermill Webworks</a>,</li>
-					<li class="vcard"><a href="http://yutmedia.com/blog" class="fn org url">Yut Media, Inc.</a>,</li>
-					<li class="vcard"><a href="http://zmanda.com/" class="fn org url">Zmanda</a>,</li>
-					<li class="vcard"><a href="http://www.zviband.com/" class="fn url">Zvi Band</a>,</li>
-					<li>you?</li>
-				</ul>
-			</div>
-			
-			<div id="actions" class="box">
-				<h3>Actions</h3>
-				<ul>
-					<!-- <li>Check out the <a href="/wiki">BarCamp DC wiki</a> for tons of information,</li> -->
-					<li>Join the <a href="http://www.flickr.com/groups/barcampdc" class="flickr">Flickr group</a> and post photos from the event,</li>
-					<li>follow us on <a href="http://www.twitter.com/barcampdc" class="twitter">Twitter</a>,</li>
-					<li>see who's <a href="http://technorati.com/search/barcampdc+OR+barcampwashingtondc" class="technorati">blogged about BarCamp DC</a>, and</li>
-					<li>learn more about the BarCamp unconference concept at the <a href="http://barcamp.org/">BarCamp site</a>.</li>
-				</ul>
-			</div>
-		</div>
-		<div id="footer">
-			<p><img src="images/icon-cc.gif" alt="Creative Commons logo" /> Some rights reserved. This site and its content are licensed under a <a href="http://creativecommons.org/licenses/by-sa/3.0/" rel="license">Creative Commons Attribution-ShareAlike 3.0 License</a>.</p>
+<!-- head -->
+	<div id="head">
+		<div class="grid_3"><span id="logo">BarCamp DC</span></div>
+		<div class="grid_2 prefix_7">
+			<div class="addthis_toolbox addthis_default_style"><a id="addthis" class="addthis_button" href="http://addthis.com/bookmark.php?v=250&amp;pub=barcampdc"><span>AddThis</span></a></div>
+			<script type="text/javascript" src="http://s7.addthis.com/js/250/addthis_widget.js#pub=barcampdc"></script>		
 		</div>
 	</div>
 
+<!-- ban -->
+	<div id="ban">
+		<div class="grid_12">
+			<h1>Next Event: Saturday, November 14, 2009, 9:30 AM - 5:30 PM</h1>
+			<h2>Martin Luther King Library, 901 G Street NW, Washington, DC 20001</h2>
+			<p><a id="reg" href="http://barcampdc3.eventbrite.com/" title="Register for this event">Register</a></p>
+			<a href="http://www.flickr.com/people/jgarber/" title="Photo credits: Jason Garber"><span id="photoCredits">Photo credits and also for a sexy good time call: Jason Garber</span></a>
+		</div>
+	</div>
+
+<!-- deets -->
+	<div id="deets">
+		<div class="grid_6">
+			<p id="mission">BarCamp is an ad-hoc gathering born from the desire for people to share and learn in an open environment. It is an intense event with discussions, demos and interaction from participants who are the main actors of the event.</p>
+			<h3>Actions</h3>
+			<ul>
+				<li>Join the <a href="http://www.flickr.com/groups/barcampdc">Flickr group</a> and post photos from the event</li>
+				<li>Follow us on <a href="http://twitter.com/barcampdc">Twitter</a></li>
+				<li>See <a href="http://technorati.com/search/?return=posts&q=barcampdc+OR+barcampwashingtondc">who's blogged</a> about BarCamp DC</li>
+				<li>Learn more about the <a href="http://barcamp.org/">BarCamp unconference</a> concept</li>
+			</ul>
+		</div>
+		<div class="grid_6">
+			<h3>A special thanks to our sponsors</h3>
+		</div>
+		<div class="grid_3">
+			<ul id="sponsors">
+				<li class="vcard"><a href="http://www.awayfind.com/">AwayFind</a></li>
+				<li class="vcard"><a href="http://www.boalt.com/">Boalt</a></li>
+				<li class="vcard"><a href="http://www.americanprogress.org/">Center for American Progress</a></li>
+				<li class="vcard"><a href="http://cleartypemedia.com/press/">ClearType Press</a></li>
+				<li class="vcard"><a href="http://twitter.com/davidsancar">David Sanchez</a></li>
+				<li class="vcard"><a href="http://www.intridea.com/">Intridea</a></li>
+				<li class="vcard"><a href="http://www.istrategylabs.com/">iStrategyLabs</a></li>
+				<li class="vcard"><a href="http://jobmatchbox.jobthread.com/">Jobmatchbox.com</a></li>
+				<li class="vcard"><a href="http://www.mobomo.com/">Mobomo</a></li>
+			</ul>
+	  </div>
+		<div class="grid_3">
+			<ul>
+				<li class="vcard"><a href="http://openhost.biz/">OpenHost</a></li>
+				<li class="vcard"><a href="http://opower.com/">OPOWER</a></li>
+				<li class="vcard"><a href="http://www.rackspacecloud.com/">The Rackspace Cloud</a></li>
+				<li class="vcard"><a href="http://www.rockcreeksm.com/">Rock Creek Strategic Marketing</a></li>
+				<li class="vcard"><a href="http://skeevisarts.com/">skeevisArts</a></li>
+				<li class="vcard"><a href="http://www.sunlightfoundation.com/">Sunlight Foundation</a></li>
+				<li class="vcard"><a href="http://www.tellenger.com/">Tellenger</a></li>
+				<li class="vcard"><a href="http://whitmoyer.com/">Whitmoyer</a></li>
+			</ul>
+		</div>
+	</div>
+	<div style="clear:both;"></div>
+	<!-- foot -->
+	<div id="foot">
+		<div class="grid_9">
+			<p id="copyright">Some rights reserved.<br/>
+				This site and its content are licensed under a <a href="http://creativecommons.org/licenses/by-sa/3.0/" rel="license">Creative Commons Attribution-ShareAlike 3.0 License</a>.</p>
+		</div>
+		<div class="grid_3">
+			<p id="Whitmoyer">Designed by <a title="Whitmoyer" href="http://whitmoyer.com/">Whitmoyer</a></p>
+		</div>
+	</div>
+	
+</div>
+<!-- end .container_12 -->
+
+<script type="text/javascript">
+	var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+	document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+	try {
+		var pageTracker = _gat._getTracker("UA-5230764-5");
+		pageTracker._setDomainName("barcampdc.org");
+		pageTracker._trackPageview();
+	} catch(err) {}
+</script>
 </body>
 </html>
