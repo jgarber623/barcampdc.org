83099a4ea08eff4956f7c7e07f90bcd5fd799432
Jason Garber <jason@sixtwothree.org>
2011-07-18 15:02:24 -0400

    Adding version 2 of BarCamp DC site to repository.

--- index.php
+++ index.php
@@ -7,116 +7,92 @@
 	<meta name="description" content="BarCamp comes to the nation's capital Saturday, August 11th, 2007." />
 	<meta name="keywords" content="barcamp dc, barcamp washington dc, barcamp, unconference, washington dc tech events, washington dc events, technology" />
 	<meta name="robots" content="all" />
-	<meta name="geo.position" content="38.903937;-77.036925" />
+	<meta name="geo.position" content="38.903762;-77.059765" />
 	<meta name="geo.region" content="US-DC" />
 	<meta name="geo.placename" content="Washington DC" />
 	<meta name="geo.country" content="US" />
 	<link rel="stylesheet" type="text/css" media="screen" href="css/barcamp.css" />
 	<script type="text/javascript" src="http://maps.google.com/maps?file=api&amp;v=2&amp;key=ABQIAAAA4vs3EKzQQ5215QbVI-kWVhS1rteLo4F7_IcxunjOqbSz4U7yBxRyoy8WuKFh6GsjkvVRsujUkTieGA"></script>
-	<script type="text/javascript" src="scripts/jquery.js"></script>
+	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.2.6/jquery.min.js"></script>
 	<script type="text/javascript" src="scripts/barcampdc.js"></script>
 </head>
 
 <body>
 
 	<div id="wrap">
-		<?php if ( isset( $_GET['merchant_return_link'] ) ) { ?>
-		<div id="thanks">
-			<p><strong>Thanks for sponsoring!</strong> We really appreciate your help! Head on over to <a href="http://barcamp.org/BarCampWashingtonDC">the wiki</a> and add yourself as a sponsor!</p>
-		</div>
-		<?php } ?>
 		<div id="header">
 			<h1><img src="images/logo.gif" alt="BarCamp DC" /></h1>
 		</div>
 		<div id="content">
 			<div id="about">
-				<!-- <p class="update vcard vevent"><strong>Update:</strong> There will be an <span class="summary">After Party</span> at <a href="http://www.cafeasia.com/" class="fn org url">Cafe Asia</a> <span class="adr">(<span class="street-address">1720 I Street NW</span>, <span class="locality">Washington</span>, <abbr class="region" title="District of Columbia">DC</abbr> <span class="postal-code">20006</span>)</span> beginning at <abbr class="dtstamp" title="2007-08-11T17:30:00Z-0500">5:30 PM</abbr>. Everyone is invited, even if you can't make it to BarCamp! Sign up at <a href="http://upcoming.yahoo.com/event/236340/">Upcoming</a>!</p> -->
 				<dl class="vevent">
 					<dt id="what">What</dt>
 						<dd><span class="summary">BarCamp DC</span> is an ad-hoc <a href="http://en.wikipedia.org/wiki/Unconference">unconference</a> and is part of the larger <a href="http://barcamp.org/">BarCamp</a> movement. While there are topics suggested for discussion, there is no set agenda until everyone gathers. Attendees are required to participate to some degree: either by giving a presentation, demo, etc. or by facilitating a group discussion. Topics are flexible, as well.</dd>
 					<dt id="when">When</dt>
-						<dd>Saturday, August 11th, 2007, <abbr class="dtstart" title="2007-08-11T09:30:00Z-0500">9:30 AM</abbr> &ndash; <abbr class="dtend" title="2007-08-11T17:30:00Z-0500">5:30 PM</abbr></dd>
+						<dd>Saturday, October 18th, 2008, <abbr class="dtstart" title="2008-10-18T08:00:00Z-0500">8:00 AM</abbr> &ndash; <abbr class="dtend" title="2008-10-18T18:00:00Z-0500">6:00 PM</abbr></dd>
 					<dt id="where">Where</dt>
 						<dd class="vcard">
-							<a href="http://www.fleishman.com/" class="fn org url">Fleishman-Hillard Inc.</a>
+							<a href="http://www.cdiabu.com/overview/washington-campus.php" class="fn org url">Center for Digital Imaging Arts at Boston University</a>
 							<span class="adr">
-								<span class="street-address">1615 L Street NW</span>
-								<span class="locality">Washington, <abbr class="region" title="District of Columbia">DC</abbr></span> <span class="postal-code">20036</span>
+								<span class="street-address">1055 Thomas Jefferson Street NW</span>
+								<span class="locality">Washington, <abbr class="region" title="District of Columbia">DC</abbr></span> <span class="postal-code">20007</span>
 								<span class="geo">
-									<span class="latitude">38.903937</span>, <span class="longitude">-77.036925</span>
+									<span class="latitude">38.903762</span>, <span class="longitude">-77.059765</span>
 								</span>
 							</span>
-							<dl id="transportation">
-								<dt>Metrorail</dt>
-									<dd>
-										<ul id="metrorail">
-											<li><a href="http://www.wmata.com/metrorail/Stations/station.cfm?station=4" class="red">Farragut North</a>,</li>
-											<li><a href="http://www.wmata.com/metrorail/Stations/station.cfm?station=38" class="blue orange">Farragut West</a>,</li>
-											<li><a href="http://www.wmata.com/metrorail/Stations/station.cfm?station=36" class="blue orange">McPherson Square</a></li>
-										</ul>
-									</dd>
-								<dt>Metrobus</dt>
-									<dd>
-										<ul id="metrobus">
-											<li>30,</li>
-											<li>32,</li>
-											<li>34,</li>
-											<li>35,</li>
-											<li>36,</li>
-											<li>38B,</li>
-											<li>42,</li>
-											<li>52,</li>
-											<li>53,</li>
-											<li>54,</li>
-											<li>80,</li>
-											<li>Circulator,</li>
-											<li>D1,</li>
-											<li>D3,</li>
-											<li>D5,</li>
-											<li>D6,</li>
-											<li>L2,</li>
-											<li>N2,</li>
-											<li>N4,</li>
-											<li>N6,</li>
-											<li>S1,</li>
-											<li>S2,</li>
-											<li>S4</li>
-										</ul>
-									</dd>
-							</dl>
 						</dd>
 				</dl>
+			</div>
+			
+			<div id="register" class="box">
+				<h3>Attend</h3>
+				<p>Want to come to BarCamp DC? Simply register at <a href="http://barcampdc2.eventbrite.com/">http://barcampdc2.eventbrite.com/</a>.</p>
 			</div>
 			
 			<div id="sponsors" class="box">
 				<h3>Sponsors</h3>
 				<ul>
-					<li class="vcard"><a href="http://200ok.net/" class="fn org url">200 OK</a>,</li>
-					<li class="vcard"><a href="http://dev.aol.com/" class="fn org url">AOL Developer Network</a>,</li>
-					<li class="vcard"><a href="http://caseysoftware.com/" class="fn org url">CaseySoftware</a>,</li>
-					<li class="vcard"><a href="http://www.clearspring.com/" class="fn org url">Clearspring Technologies</a>,</li>
-					<li class="vcard"><a href="http://www.culturecaptioning.com/" class="fn org url">Culture Captioning</a>,</li>
-					<li class="vcard"><a href="http://www.digitalphenom.com/" class="fn org url">Digital Phenom</a>,</li>
-					<li class="vcard"><a href="http://www.fleishman.com/" class="fn org url">Fleishman-Hillard</a>,</li>
-					<li class="vcard"><a href="http://www.freewebs.com/" class="fn org url">Freewebs</a>,</li>
-					<li class="vcard"><a href="http://www.jfciii.com/" class="fn url">John F Croston III</a>,</li>
-					<li class="vcard"><a href="http://www.jounce.net/" class="fn org url">Jounce</a>,</li>
-					<li class="vcard"><a href="http://lawver.net/" class="fn url">Kevin Lawver</a>,</li>
-					<li class="vcard"><a href="http://dc.metblogs.com/" class="fn org url">Metroblogging DC</a>,</li>
-					<li class="vcard"><a href="http://www.microsoft.com/" class="fn org url">Microsoft</a>,</li>
-					<li class="vcard"><a href="http://nclud.com/" class="fn org url">nclud</a>,</li>
-					<li class="vcard"><a href="http://www.setconsulting.com/" class="fn org url">SET Consulting</a>,</li>
-					<li class="vcard"><a href="http://www.sunlightfoundation.com/" class="fn org url">Sunlight Foundation</a>,</li>
-					<li class="vcard"><a href="http://www.viget.com/" class="fn org url">Viget Labs</a></li>
+					<li class="vcard"><a href="http://1piazza.com/" class="fn org url">1Piazza</a>,</li>
+					<li class="vcard"><a href="http://www.au.org/" class="fn org url">Americans United for Separation of Church and State</a>,</li>
+					<li class="vcard"><a href="http://www.ask500people.com/" class="fn org url">Ask500People</a>,</li>
+					<li class="vcard"><a href="http://www.awayfind.com/" class="fn org url">AwayFind</a>,</li>
+					<li class="vcard"><a href="http://blackmesh.com/" class="fn org url">BlackMesh</a>,</li>
+					<li class="vcard"><a href="http://caseysoftware.com/" class="fn org url">CaseySoftware, LLC</a>,</li>
+					<li class="vcard"><a href="http://www.collectivedetective.com/" class="fn org url">Collective Detective</a>,</li>
+					<li class="vcard"><a href="http://www.contribune.org/" class="fn org url">Contribune</a>,</li>
+					<li class="vcard"><a href="http://www.designreinvented.com/" class="fn org url">Design Reinvented</a>,</li>
+					<li class="vcard"><a href="http://friendcompare.com/" class="fn org url">FriendCompare</a>,</li>
+					<li class="vcard"><a href="http://intridea.com/" class="fn org url">Intridea</a>,</li>
+					<li class="vcard"><a href="http://www.introin.com/" class="fn org url">IntroIn</a>,</li>
+					<li class="vcard"><a href="http://istrategylabs.com/" class="fn org url">iStrategyLabs</a>,</li>
+					<li class="vcard"><a href="http://www.iterativedesigns.com/" class="fn org url">Iterative Designs</a>,</li>
+					<li class="vcard"><a href="http://jess3.com/" class="fn org url">Jess3</a>,</li>
+					<li class="vcard"><a href="http://jobmatchbox.jobthread.com/" class="fn org url">Jobmatchbox.com</a>,</li>
+					<li class="vcard"><a href="http://www.jfciii.com/blog/" class="fn url">John F Croston III</a>,</li>
+					<li class="vcard"><a href="http://jounce.net/" class="fn org url">Jounce</a>,</li>
+					<li class="vcard"><a href="http://www.lesliebradshaw.com/" class="fn url">Leslie Bradshaw</a>,</li>
+					<li class="vcard"><a href="http://m.odul.us/" class="fn org url">Matt Bowen</a>,</li>
+					<li class="vcard"><a href="http://www.networksolutions.com/" class="fn org url">Network Solutions</a>,</li>
+					<li class="vcard"><a href="http://openhost.biz/" class="fn org url">OpenHost</a>,</li>
+					<li class="vcard"><a href="http://www.positiveenergyusa.com/" class="fn org url">Positive Energy</a>,</li>
+					<li class="vcard"><a href="http://potomactechwire.com/" class="fn org url">PotomacTechWire</a>,</li>
+					<li class="vcard"><a href="http://sharememe.com/" class="fn org url">ShareMeme</a>,</li>
+					<li class="vcard"><a href="http://siteworx.com/" class="fn org url">Siteworx</a>,</li>
+					<li class="vcard"><a href="http://www.viget.com/" class="fn org url">Viget Labs</a>,</li>
+					<li class="vcard"><a href="http://www.welovedc.com/" class="fn org url">We Love DC</a>,</li>
+					<li class="vcard"><a href="http://www.wondermill.com/" class="fn org url">Wondermill Webworks</a>,</li>
+					<li class="vcard"><a href="http://yutmedia.com/blog" class="fn org url">Yut Media, Inc.</a>,</li>
+					<li class="vcard"><a href="http://zmanda.com/" class="fn org url">Zmanda</a>,</li>
+					<li class="vcard"><a href="http://www.zviband.com/" class="fn url">Zvi Band</a>,</li>
+					<li>you?</li>
 				</ul>
 			</div>
 			
 			<div id="actions" class="box">
 				<h3>Actions</h3>
 				<ul>
-					<li>Check out the <a href="/wiki">BarCamp DC wiki</a> for tons of information,</li>
-					<li>mark yourself as "attending" on the <a href="http://upcoming.yahoo.com/event/211115/" class="upcoming">Upcoming event page</a>,</li>
-					<li>join the <a href="http://www.flickr.com/groups/barcampdc" class="flickr">Flickr group</a> and post photos from the event,</li>
+					<!-- <li>Check out the <a href="/wiki">BarCamp DC wiki</a> for tons of information,</li> -->
+					<li>Join the <a href="http://www.flickr.com/groups/barcampdc" class="flickr">Flickr group</a> and post photos from the event,</li>
 					<li>follow us on <a href="http://www.twitter.com/barcampdc" class="twitter">Twitter</a>,</li>
 					<li>see who's <a href="http://technorati.com/search/barcampdc+OR+barcampwashingtondc" class="technorati">blogged about BarCamp DC</a>, and</li>
 					<li>learn more about the BarCamp unconference concept at the <a href="http://barcamp.org/">BarCamp site</a>.</li>
--- scripts/barcampdc.js
+++ scripts/barcampdc.js
@@ -1,65 +1,23 @@
 var barcampdc = {
-	barcamp: { lat: 38.903937, lng: -77.036925 },
-	metrorail: [
-		{ name: "Farragut North", lat: 38.903192, lng: -77.039766 },
-		{ name: "Farragut West", lat: 38.901311, lng: -77.03981 },
-		{ name: "McPherson Square", lat: 38.901316, lng: -77.033652 }
-	],
-	
-	showMetroLineIcons: function() {
-		$( "ul#metrorail li a", "dl#transportation" ).each( function( i ) {
-			var anchor = $( this );
-			var pieces = anchor.attr( "class" ).split( " " );
-			
-			$.each( pieces, function( j, n ) {
-				anchor.before( '<img src="images/icon-metro-' + n + '.gif" alt="" title="This stop is on the ' + n + ' line" />' );
-			});
-		});
-	},
-	
-	showSiteFavicons: function() {
-		$.each( ["a.flickr", "a.technorati", "a.twitter", "a.upcoming"], function( i, n ) {
-			$( n ).before( '<img src="images/icon-' + $( n ).attr( "class" ) + '.gif" alt="" />' );
-		});
-	},
-	
 	showGoogleMap: function() {
 		if ( !GBrowserIsCompatible() ) { return; }
 		
-		var map, barcamp_icon, metro_icon, barcamp_point, point;
-		$( "dt#where" ).next().prepend( '<div id="map"></div>' );
+		var dt = $( "dt#where" ), dd = dt.next(), geo = $( "span.geo", dd ), coords = { lat: parseFloat( $( "span.latitude", geo ).text() ), lng: parseFloat( $( "span.longitude", geo ).text() ) };
+		dd.prepend( '<div id="map"></div>' );
 		
-		map = new GMap2( document.getElementById( "map" ) );	
+		var map = new GMap2( document.getElementById( "map" ) );	
 		map.addControl( new GSmallMapControl() );
-		map.setCenter( new GLatLng( this.barcamp.lat, this.barcamp.lng ), 15 );
+		map.setCenter( new GLatLng( coords.lat, coords.lng ), 15 );
 		
-		barcamp_icon = new GIcon();
-		// barcamp_icon.image = "http://localhost/barcampdc/images/icon-barcamp.gif";
-		barcamp_icon.image = "http://barcampdc.org/images/icon-barcamp.gif";
-		barcamp_icon.iconSize = new GSize( 23, 28 );
-		barcamp_icon.iconAnchor = new GPoint( 11, 14 );
+		var icon = new GIcon();
+		icon.image = "http://barcampdc.org/images/icon-barcamp.gif";
+		icon.iconSize = new GSize( 23, 28 );
+		icon.iconAnchor = new GPoint( 11, 14 );
 		
-		metro_icon = new GIcon();
-		// metro_icon.image = "http://localhost/barcampdc/images/icon-metro.gif";
-		metro_icon.image = "http://barcampdc.org/images/icon-metro.gif";
-		metro_icon.iconSize = new GSize( 19, 23 );
-		metro_icon.iconAnchor = new GPoint( 9, 11 );
-		
-		barcamp_point = new GLatLng( this.barcamp.lat, this.barcamp.lng );
-		map.addOverlay( new GMarker( barcamp_point, barcamp_icon ) );
-		
-		$.each( this.metrorail, function( i, n ) {
-			point = new GLatLng( n.lat, n.lng )
-			map.addOverlay( new GMarker( point, metro_icon ) );
-		});
-	},
-	
-	init: function() {
-		this.showMetroLineIcons();
-		// this.showSiteFavicons();
-		this.showGoogleMap();
+		var point = new GLatLng( coords.lat, coords.lng );
+		map.addOverlay( new GMarker( point, icon ) );
 	}
 }
 
-$().ready( function() { barcampdc.init(); } );
+$().ready( function() { barcampdc.showGoogleMap(); } );
 $().unload( GUnload() );
